20f43997bfd3f9776c041377e1b9dd0db5be70d6
Jan Smolka <onkelsmo@gmail.com>
2014-12-24 16:01:01 +0100

    ADD: Activity for handling the top action bar

--- app/src/main/java/mediasmo/mygiftmind/DisplayGiftsActivity.java
+++ app/src/main/java/mediasmo/mygiftmind/DisplayGiftsActivity.java
@@ -1,50 +1,11 @@
 package mediasmo.mygiftmind;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class DisplayGiftsActivity extends ActionBarActivity {
-
+public class DisplayGiftsActivity extends GlobalActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_gifts);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-//        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_contacts:
-                openContacts();
-                return true;
-            case R.id.action_gifts:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void openContacts() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
 }
---
+++ app/src/main/java/mediasmo/mygiftmind/GlobalActionBarActivity.java
@@ -0,0 +1,173 @@
+package mediasmo.mygiftmind;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+abstract public class GlobalActionBarActivity extends ActionBarActivity {
+    private DrawerLayout menuDrawerLayout;
+    private ListView menuDrawerList;
+    private ActionBarDrawerToggle menuDrawerToggle;
+    private CharSequence menuDrawerTitle;
+    private CharSequence menuTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String[] menuTitles;
+
+        menuTitle = menuDrawerTitle = getTitle();
+        menuTitles = getResources().getStringArray(R.array.menu_titles);
+        menuDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        menuDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        menuDrawerList.setAdapter(new ArrayAdapter<>(this,
+                R.layout.drawer_list_item, menuTitles));
+        // Set the list's click listener
+        menuDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // Enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        menuDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                menuDrawerLayout,         /* DrawerLayout object */
+                R.drawable.abc_ic_menu_moreoverflow_mtrl_alpha,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(menuTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(menuDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        menuDrawerLayout.setDrawerListener(menuDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        //return true;
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (menuDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        // Handle action buttons
+        switch (item.getItemId()) {
+            case R.id.action_contacts:
+                openContacts();
+                return true;
+            case R.id.action_gifts:
+                openGifts();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openGifts() {
+        Intent intent = new Intent(this, DisplayGiftsActivity.class);
+        startActivity(intent);
+    }
+
+    private void openContacts() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    /* The click listener for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // Update the main content by replacing fragments
+        Fragment fragment = new ContentFragment();
+        Bundle args = new Bundle();
+        args.putInt(ContentFragment.ARG_CONTENT_ID, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // Update selected item and title, then close the drawer
+        menuDrawerList.setItemChecked(position, true);
+        //setTitle(menuTitles[position]);
+        menuDrawerLayout.closeDrawer(menuDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        menuTitle = title;
+        getSupportActionBar().setTitle(menuTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        menuDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        menuDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    public static class ContentFragment extends Fragment {
+        public static final String ARG_CONTENT_ID = "content_id";
+
+        public ContentFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
+            //int i = getArguments().getInt(ARG_CONTENT_ID);
+            //String content = getResources().getStringArray(R.array.menu_titles)[i];
+
+            return rootView;
+
+        }
+    }
+}
