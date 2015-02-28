d8b4c4241f6d07ff0717fc3138b6c030c1251396
onkelsmo <onkelsmo@gmail.com>
2015-02-28 19:09:33 +0100

    ADD: onCreate and onUpgrade methods in databaseHandler

--- app/src/main/java/mediasmo/mygiftmind/helper/DatabaseHandler.java
+++ app/src/main/java/mediasmo/mygiftmind/helper/DatabaseHandler.java
@@ -35,16 +35,30 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    // TODO: implement overriden methods and CRUD Operations
-    // http://www.androidhive.info/2011/11/android-sqlite-database-tutorial/
-
+    /**
+     * onCreate
+     *
+     * @param db SQLiteDatabase
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
+                + KEY_ID + "INTEGER PRIMARY KEY, " + KEY_NAME + " TEXT)";
+        db.execSQL(CREATE_CONTACTS_TABLE);
     }
 
+    /**
+     * onUpgrade
+     *
+     * @param db SQLiteDatabase
+     * @param oldVersion int
+     * @param newVersion int
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        // create tables again
+        onCreate(db);
     }
 }
